303ec6861afb5f18bfb6d7fd8c43644fea1c6f6e
Saidulhimu <saidulislam5595@gmail.com>
2026-09-11 04:36:52 +0600

    implement spinner  .

--- src/components/common/Spinner.tsx
+++ src/components/common/Spinner.tsx
@@ -1,11 +1,9 @@
-import React from 'react';
-
 const Spinner = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+  return (
+    <div className="flex justify-center items-center py-20">
+      <div className="animate-spin rounded-full h-12 w-12 border-t-2 border-b-2 border-purple-600"></div>
+    </div>
+  );
 };
 
 export default Spinner;
